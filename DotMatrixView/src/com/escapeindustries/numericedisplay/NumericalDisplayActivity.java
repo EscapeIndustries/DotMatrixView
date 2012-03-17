365c728dead3447cfdc3cb9846ceea5a7546382c
Mark Roberts <mark@taurine.org.uk>
2012-03-17 15:17:21 +0000

    Created empty project - no work done yet.

---
+++ DotMatrixView/src/com/escapeindustries/numericedisplay/NumericalDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.escapeindustries.numericedisplay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NumericalDisplayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
