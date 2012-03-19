9b8fbd90038b2e730626f6313b8eb727df4e46ef
Mark Roberts <mark@taurine.org.uk>
2012-03-19 00:05:25 +0000

    Built out lots of experimental matrix layouts. Started writing code to explore how I can find and change dots.

--- DotMatrixView/src/com/escapeindustries/numericedisplay/NumericalDisplayActivity.java
+++ DotMatrixView/src/com/escapeindustries/numericedisplay/NumericalDisplayActivity.java
@@ -2,12 +2,45 @@ package com.escapeindustries.numericedisplay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class NumericalDisplayActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.matrix);
+		ViewGroup matrix = (ViewGroup) findViewById(R.id.matrix);
+
+		// Experiment to show you can find the items in the grid predictably
+		int count = matrix.getChildCount();
+		Log.d("NumericalDisplayActivity", "count == " + count);
+		for (int i = 0; i < count; i++) {
+			View item = matrix.getChildAt(i);
+			Log.d("NumericalDisplayActivity",
+					""
+							+ i
+							+ ": "
+							+ (item instanceof ViewGroup ? (item instanceof LinearLayout ? "LinearLayout"
+									: "Viewgroup")
+									: "View"));
+			int itemCount = ((ViewGroup) item).getChildCount();
+			Log.d("NumericalDisplay", "itemCount == " + itemCount);
+			for (int j = 0; j < itemCount; j++) {
+				View subItem = ((ViewGroup) item).getChildAt(j);
+				Log.d("NumericalDisplayActivity", "" + ((i * itemCount) + j)
+						+ ": "
+						+ (subItem instanceof ImageView ? "ImageView" : "View"));
+			}
+		}
+
+		// Experiment to show you can turn one from drim to bright
+		ImageView changeMe = (ImageView) ((ViewGroup) matrix.getChildAt(0))
+				.getChildAt(0);
+		changeMe.setImageResource(R.drawable.dot_lit);
+	}
 }
