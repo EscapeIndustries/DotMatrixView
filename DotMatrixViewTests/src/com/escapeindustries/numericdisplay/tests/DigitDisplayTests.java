a6f045138664895599f7bf235793d5377a4260cb
Mark Roberts <mark@taurine.org.uk>
2012-04-05 13:33:19 +0100

    Changes to reflect refactoring made to the code under test.

--- DotMatrixViewTests/src/com/escapeindustries/numericdisplay/tests/DigitDisplayTests.java
+++ DotMatrixViewTests/src/com/escapeindustries/numericdisplay/tests/DigitDisplayTests.java
@@ -22,7 +22,7 @@ public class DigitDisplayTests extends TestCase {
 		int[] expected = { 55, 62, 69, 76, 83 };
 		DigitTransition trans = new DigitTransition(one, two);
 		// Act
-		int[] result = trans.getTransitionToDimDots();
+		int[] result = trans.getDotsToDim();
 		// Assert
 		assertEquals("Number of dots to dim", expected.length, result.length);
 		for (int i = 0; i < expected.length; i++) {
@@ -36,7 +36,7 @@ public class DigitDisplayTests extends TestCase {
 		int[] expected = { 1, 2, 3, 4, 5, 43, 44, 45, 46, 47, 49, 56, 63, 70, 77, 85, 86, 87, 88, 89 };
 		DigitTransition trans = new DigitTransition(one, two);
 		// Act
-		int[] result = trans.getTransitionToLitDots();
+		int[] result = trans.getDotsToLight();
 		// Assert
 		assertEquals("Number of dots to be lit", expected.length, result.length);
 		for (int i = 0; i < expected.length; i++) {
