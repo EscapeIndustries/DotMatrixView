cc9b10d8fee8d0450414930ff3c300664dc9fcf6
Mark Roberts <mark@taurine.org.uk>
2012-04-06 14:01:56 +0100

    Added tests for more transition cases.

--- DotMatrixViewTests/src/com/escapeindustries/numericdisplay/tests/DigitDisplayTests.java
+++ DotMatrixViewTests/src/com/escapeindustries/numericdisplay/tests/DigitDisplayTests.java
@@ -1,7 +1,6 @@
 package com.escapeindustries.numericdisplay.tests;
 
 import com.escapeindustries.numericedisplay.Digit;
-import com.escapeindustries.numericedisplay.DigitDisplay;
 import com.escapeindustries.numericedisplay.DigitTransition;
 
 import junit.framework.TestCase;
@@ -10,14 +9,16 @@ public class DigitDisplayTests extends TestCase {
 
 	private int[] one;
 	private int[] two;
+	private int[] three;
 
 	@Override
 	protected void setUp() throws Exception {
 		one = Digit.digitPatterns[1];
 		two = Digit.digitPatterns[2];
+		three = Digit.digitPatterns[3];
 	}
 
-	public void testFindCorrectDotsToDim() {
+	public void testFindCorrectDotsToDimFromEndsBeforeTo() {
 		// Arrange
 		int[] expected = { 55, 62, 69, 76, 83 };
 		DigitTransition trans = new DigitTransition(one, two);
@@ -30,17 +31,76 @@ public class DigitDisplayTests extends TestCase {
 					result[i]);
 		}
 	}
-	
-	public void testFindCorrectDotsToLight() {
+
+	public void testFindCorrectDotsToDimToEndsBeforeFrom() {
 		// Arrange
-		int[] expected = { 1, 2, 3, 4, 5, 43, 44, 45, 46, 47, 49, 56, 63, 70, 77, 85, 86, 87, 88, 89 };
+		int[] expected = { 1, 2, 3, 4, 5, 43, 44, 45, 46, 47, 49, 56, 63, 70,
+				77, 85, 86, 87, 88, 89 };
+		DigitTransition trans = new DigitTransition(two, one);
+		// Act
+		int[] result = trans.getDotsToDim();
+		// Assert
+		assertEquals("Number of dots to dim", expected.length, result.length);
+		for (int i = 0; i < expected.length; i++) {
+			assertEquals("Dots to be dimmed meet expectations", expected[i],
+					result[i]);
+		}
+	}
+
+	public void testFindCorrectDotsToDimToAndFromEndSameDot() {
+		// Arrange
+		int[] expected = { 49, 56, 63, 70, 77 };
+		DigitTransition trans = new DigitTransition(two, three);
+		// Act
+		int[] result = trans.getDotsToDim();
+		// Assert
+		assertEquals("Number of dots to dim", expected.length, result.length);
+		for (int i = 0; i < expected.length; i++) {
+			assertEquals("Dots to be dimmed meet expectations", expected[i],
+					result[i]);
+		}
+	}
+
+	public void testFindCorrectDotsToLightFromEndsBeforeTo() {
+		// Arrange
+		int[] expected = { 1, 2, 3, 4, 5, 43, 44, 45, 46, 47, 49, 56, 63, 70,
+				77, 85, 86, 87, 88, 89 };
 		DigitTransition trans = new DigitTransition(one, two);
 		// Act
 		int[] result = trans.getDotsToLight();
 		// Assert
 		assertEquals("Number of dots to be lit", expected.length, result.length);
 		for (int i = 0; i < expected.length; i++) {
-			assertEquals("Dots to be lit meet expectations", expected[i], result[i]);
+			assertEquals("Dots to be lit meet expectations", expected[i],
+					result[i]);
+		}
+	}
+
+	public void testFindCorrectDotsToLightToEndsBeforeFrom() {
+		// Arrange
+		int[] expected = { 55, 62, 69, 76, 83 };
+		DigitTransition trans = new DigitTransition(two, one);
+		// Act
+		int[] result = trans.getDotsToLight();
+		// Assert
+		assertEquals("Number of dots to be lit", expected.length, result.length);
+		for (int i = 0; i < expected.length; i++) {
+			assertEquals("Dots to be lit meet expectations", expected[i],
+					result[i]);
+		}
+	}
+
+	public void testFindCorrectDotsToLightToAndFromEndSameDot() {
+		// Arrange
+		int[] expected = { 55, 62, 69, 76, 83 };
+		DigitTransition trans = new DigitTransition(two, three);
+		// Act
+		int[] result = trans.getDotsToLight();
+		// Assert
+		assertEquals("Number of dots to be lit", expected.length, result.length);
+		for (int i = 0; i < expected.length; i++) {
+			assertEquals("Dots to be lit meet expectations", expected[i],
+					result[i]);
 		}
 	}
 }
