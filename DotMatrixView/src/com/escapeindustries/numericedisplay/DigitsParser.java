b65eef66d52f6cc57cbc3d1628f4d9f262368fdd
Mark Roberts <mark@taurine.org.uk>
2012-04-13 13:19:46 +0100

    Began code to parse Strings into individual digits so they can be used to configure the display.

---
+++ DotMatrixView/src/com/escapeindustries/numericedisplay/DigitsParser.java
@@ -0,0 +1,13 @@
+package com.escapeindustries.numericedisplay;
+
+public class DigitsParser {
+
+	public int[] parse(String input) {
+		// Find out if this has already been implemented as some sort of NumberFormat class in the standard library
+		// Otherwise use a regular expression to validate? Or just return something bad if it fails validation while
+		// parsing?
+		
+		return new int[0];
+	}
+
+}
