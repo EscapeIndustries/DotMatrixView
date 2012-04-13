c0f805a7528f0bf7cb5d93c5c09f102c91e6a499
Mark Roberts <mark@taurine.org.uk>
2012-04-13 13:18:27 +0100

    Wrote first test to support digit parsing from String.

---
+++ DotMatrixViewTests/src/com/escapeindustries/numericdisplay/tests/DigitParserTests.java
@@ -0,0 +1,35 @@
+package com.escapeindustries.numericdisplay.tests;
+
+import com.escapeindustries.numericedisplay.DigitsParser;
+
+import junit.framework.TestCase;
+
+public class DigitParserTests extends TestCase {
+
+	private DigitsParser parser;
+
+	@Override
+	protected void setUp() throws Exception {
+		parser = new DigitsParser();
+	}
+	
+	public void testBasicParse() {
+		// Arrange
+		int[] expected = { 0, 0, 0, 0};
+		// Act
+		int[] result = parser.parse("00:00");
+		// Assert
+		assertTrue("Parsed 4 digits with colon",
+				intArraysAreEqual(expected, result));
+	}
+
+	private boolean intArraysAreEqual(int[] expected, int[] result) {
+		if (expected.length != result.length)
+			return false;
+		for (int i = 0; i < expected.length; i++) {
+			if (expected[i] != result[i])
+				return false;
+		}
+		return true;
+	}
+}
