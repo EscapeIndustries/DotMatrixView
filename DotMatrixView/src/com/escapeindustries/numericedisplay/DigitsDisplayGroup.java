bb419b37f812048d525e12791e6933f18a8126f2
Mark Roberts <mark@taurine.org.uk>
2012-04-17 13:20:08 +0100

    Abstracted digit group from activity into its own class.

---
+++ DotMatrixView/src/com/escapeindustries/numericedisplay/DigitsDisplayGroup.java
@@ -0,0 +1,43 @@
+package com.escapeindustries.numericedisplay;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class DigitsDisplayGroup {
+
+	private ViewGroup[] digitViewGroups;
+	private DigitDisplay[] digits;
+
+	public DigitsDisplayGroup(Context ctx, ViewGroup parent) {
+		digitViewGroups = new ViewGroup[4];
+		for (int i = 0; i < parent.getChildCount(); i++) {
+			// Dirty hack to ensure we only get the digits and not
+			// the colon between digits 2 and 3
+			// AND ignore the spacer columns!
+			if (i % 2 == 0) {
+				if (i < 4) {
+					digitViewGroups[i / 2] = (ViewGroup) parent.getChildAt(i);
+				} else if (i > 4) {
+					digitViewGroups[(i / 2) - 1] = (ViewGroup) parent
+							.getChildAt(i);
+				}
+			}
+		}
+		digits = new DigitDisplay[4];
+		for (int i = 0; i < digits.length; i++) {
+			digits[i] = new DigitDisplay(ctx, digitViewGroups[i]);
+		}
+	}
+
+	public void setValue(String value) {
+		DigitsParser parser = new DigitsParser();
+		int[] values = parser.parse(value);
+		for (int i = 0; i < values.length; i++) {
+			digits[i].setNumber(values[i]);
+		}
+	}
+
+	public void incrementEachSecond() {
+		new DigitIncrementTask(digits[3]).execute("");
+	}
+}
--- DotMatrixView/src/com/escapeindustries/numericedisplay/NumericalDisplayActivity.java
+++ DotMatrixView/src/com/escapeindustries/numericedisplay/NumericalDisplayActivity.java
@@ -17,63 +17,12 @@ public class NumericalDisplayActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.clock);
-		ViewGroup digitsGroup = (ViewGroup) findViewById(R.id.digits);
-		// animateAllLightableDots(digits);
+		DigitsDisplayGroup digitsGroup = new DigitsDisplayGroup(this,
+				(ViewGroup) findViewById(R.id.digits));
+		
+		digitsGroup.setValue("00:01");
+		digitsGroup.incrementEachSecond();
 
-		ViewGroup[] digitsOnly = new ViewGroup[4];
-		for (int i = 0; i < digitsGroup.getChildCount(); i++) {
-			// Dirty hack to ensure we only get the digits and not
-			// the colon between digits 2 and 3
-			// AND ignore the spacer columns!
-			if (i % 2 == 0) {
-				if (i < 4) {
-					digitsOnly[i / 2] = (ViewGroup) digitsGroup.getChildAt(i);
-				} else if (i > 4) {
-					digitsOnly[(i / 2) - 1] = (ViewGroup) digitsGroup
-							.getChildAt(i);
-				}
-			}
-		}
-
-		DigitDisplay[] digits = new DigitDisplay[4];
-		for (int i = 0; i < digits.length; i++) {
-			digits[i] = new DigitDisplay(this, digitsOnly[i]);
-		}
-
-		DigitsParser parser = new DigitsParser();
-		int[] values = parser.parse("00:01");
-		for (int i = 0; i < values.length; i++) {
-			digits[i].setNumber(values[i]);
-		}
-
-		// new DigitIncrementTask(one).execute("");
-		// new DigitIncrementTask(two).execute("");
-		// new DigitIncrementTask(three).execute("");
-		new DigitIncrementTask(digits[3]).execute("");
-
-	}
-
-	private void animateAllLightableDots(ViewGroup digits) {
-		for (int digit = 0; digit < digits.getChildCount(); digit++) {
-
-			ViewGroup matrix = (ViewGroup) digits.getChildAt(digit);
-
-			// Experiment to show you can find the items in the grid predictably
-			int count = matrix.getChildCount();
-			for (int i = 0; i < count; i++) {
-				View item = matrix.getChildAt(i);
-				int itemCount = ((ViewGroup) item).getChildCount();
-				for (int j = 0; j < itemCount; j++) {
-					View subItem = ((ViewGroup) item).getChildAt(j);
-					if (subItem instanceof FrameLayout) {
-						// Only the items inside FrameLayouts need to animate
-						ImageView dot = (ImageView) ((ViewGroup) subItem)
-								.getChildAt(1);
-						fadeOut(dot);
-					}
-				}
-			}
-		}
 	}
 
 	private void fadeOut(ImageView dot) {
