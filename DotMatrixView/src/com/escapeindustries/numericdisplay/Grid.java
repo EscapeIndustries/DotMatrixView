6aef386777af84a42bff2d74732acc016a69293e
Mark Roberts <mark@taurine.org.uk>
2012-05-02 14:02:55 +0100

    Set layout parameters on all views that are part of the grid.

--- DotMatrixView/src/com/escapeindustries/numericdisplay/Grid.java
+++ DotMatrixView/src/com/escapeindustries/numericdisplay/Grid.java
@@ -3,7 +3,9 @@ package com.escapeindustries.numericdisplay;
 import com.escapeindustries.numericedisplay.R;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -13,6 +15,8 @@ public class Grid {
 	private int rows;
 	private ViewGroup grid;
 	private Context ctx;
+	private int dotPadding;
+	private int dotSize;
 
 	// Sizing for the existing display
 	// digit digit digit digit digit digit = 6 x 7
@@ -33,12 +37,16 @@ public class Grid {
 		this.ctx = ctx;
 		this.columns = columns;
 		this.rows = rows;
+		this.dotSize = (int) ctx.getResources().getDimension(R.dimen.dot_size);
+		this.dotPadding = (int) ctx.getResources().getDimension(
+				R.dimen.dot_padding);
 		buildGrid();
 	}
 
 	private void buildGrid() {
 		grid = new LinearLayout(ctx);
-		((LinearLayout)grid).setOrientation(LinearLayout.VERTICAL);
+		((LinearLayout) grid).setOrientation(LinearLayout.VERTICAL);
+		setLayoutWrapContent(grid);
 		for (int y = 0; y < rows; y++) {
 			grid.addView(buildRow());
 		}
@@ -46,6 +54,7 @@ public class Grid {
 
 	private LinearLayout buildRow() {
 		LinearLayout row = new LinearLayout(ctx);
+		setLayoutWrapContent(row);
 		row.setOrientation(LinearLayout.HORIZONTAL);
 		for (int x = 0; x < columns; x++) {
 			row.addView(buildDotStack());
@@ -55,13 +64,25 @@ public class Grid {
 
 	private FrameLayout buildDotStack() {
 		FrameLayout frame = new FrameLayout(ctx);
+		setLayoutWrapContent(frame);
 		ImageView dot = new ImageView(ctx);
+		setLayoutFromDimens(dot);
 		dot.setImageResource(R.drawable.dot_dim);
 		frame.addView(dot);
 		dot = new ImageView(ctx);
+		setLayoutFromDimens(dot);
 		dot.setImageResource(R.drawable.dot_lit);
 		frame.addView(dot);
 		return frame;
 	}
 
+	private void setLayoutWrapContent(View view) {
+		view.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT));
+	}
+
+	private void setLayoutFromDimens(View view) {
+		view.setLayoutParams(new LayoutParams(dotSize, dotSize));
+		view.setPadding(dotPadding, dotPadding, dotPadding, dotPadding);
+	}
 }
