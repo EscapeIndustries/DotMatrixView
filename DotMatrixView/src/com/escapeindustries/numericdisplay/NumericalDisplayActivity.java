10ead3287b7ec236a274d944491b6c03953cac1c
Mark Roberts <mark@taurine.org.uk>
2012-05-08 13:25:31 +0100

    Tidied up code removing unused code and imports.

--- DotMatrixView/src/com/escapeindustries/numericdisplay/NumericalDisplayActivity.java
+++ DotMatrixView/src/com/escapeindustries/numericdisplay/NumericalDisplayActivity.java
@@ -1,19 +1,9 @@
 package com.escapeindustries.numericdisplay;
 
 import com.escapeindustries.numericedisplay.R;
-import com.escapeindustries.numericedisplay.R.anim;
-import com.escapeindustries.numericedisplay.R.id;
-import com.escapeindustries.numericedisplay.R.layout;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 
 public class NumericalDisplayActivity extends Activity {
 
@@ -27,15 +17,5 @@ public class NumericalDisplayActivity extends Activity {
 		
 		digitsGroup.updateFromClockEachSecond();
 
-	}
-
-	private void fadeOut(ImageView dot) {
-		Animation vanish = AnimationUtils.loadAnimation(this, R.anim.vanish);
-		Animation appear = AnimationUtils.loadAnimation(this, R.anim.appear);
-		AnimationListener vanishChain = new ChainedListener(dot, appear);
-		AnimationListener appearChain = new ChainedListener(dot, vanish);
-		vanish.setAnimationListener(vanishChain);
-		appear.setAnimationListener(appearChain);
-		dot.startAnimation(vanish);
 	}
 }
