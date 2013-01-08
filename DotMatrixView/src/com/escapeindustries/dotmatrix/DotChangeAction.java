20d5e0362ce6a124c117c6e9cda0c1f05ebd7777
Mark Roberts <mark@taurine.org.uk>
2013-01-08 14:07:31 +0000

    Added JavaDoc to DotChangeAction

--- DotMatrixView/src/com/escapeindustries/dotmatrix/DotChangeAction.java
+++ DotMatrixView/src/com/escapeindustries/dotmatrix/DotChangeAction.java
@@ -1,5 +1,18 @@
 package com.escapeindustries.dotmatrix;
 
+/**
+ * Pluggable action called by {@link GlyphTransition#makeTransition} when a dot
+ * changes, allowing alternative actions to be swapped in. This was introduced
+ * to aid testability.
+ * 
+ * @author Mark Roberts
+ * 
+ */
 public interface DotChangeAction {
+	/**
+	 * Perform an action when a dot changes
+	 * @param index The one dimensional coordinate of the dot that has changed
+	 * @param on The new state of the dot
+	 */
 	public void dotHasChanged(int index, boolean on);
 }
